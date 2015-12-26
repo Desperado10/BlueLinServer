141f2c92fb2bb00aa2fadce89f648534ed6d37e6
Desperado10 <konrad.desperado@gmx.de>
2015-12-26 14:06:41 +0100

    Added setup.sh

---
+++ setup.sh
@@ -0,0 +1,2 @@
+tar -C ./Toolchain/ -xf ./Toolchain/gcc-linaro-arm-linux-gnueabihf-4.9-2014.09_linux.tar.xz
+mv ./Toolchain/gcc-linaro-arm-linux-gnueabihf-4.9-2014.09_linux ./Toolchain/bbb_toolchain
